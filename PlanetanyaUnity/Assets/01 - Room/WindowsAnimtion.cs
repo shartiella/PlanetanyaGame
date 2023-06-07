4283fe18bafa115a90529a53e2581ec785bbf1d6
shartiella <shartiella@gmail.com>
2023-06-07 13:12:24 +0300

    לוגו

--- PlanetanyaUnity/Assets/01 - Room/WindowsAnimtion.cs
+++ PlanetanyaUnity/Assets/01 - Room/WindowsAnimtion.cs
@@ -1,25 +1,36 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WindowsAnimtion : MonoBehaviour
 {
+    private Transform window;
+    private Vector3 initialPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        window = GetComponent<Transform>();
+        initialPosition = window.localPosition;
+        //initialPosition = window.position;
+        Debug.Log("to "+initialPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("position "+window.localPosition);
     }
 
     private void OnEnable()
     {
-      transform.localPosition=new Vector3(0,-Screen.height,0);
-        transform.LeanMoveLocalY(0, 1);
+        Vector3 downPosition = new Vector3(0, -Screen.height, 0);
+        Debug.Log("from "+downPosition);
+        transform.localPosition = downPosition;
+        //transform.position = downPosition;
+        transform.LeanMoveLocal(initialPosition, 1);
+        //transform.LeanMove(initialPosition, 1);
+
     }
 
 
