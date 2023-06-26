d72a71f68da80519b805b1e9230254a79a83fe8f
shartiella <shartiella@gmail.com>
2023-06-26 14:34:28 +0300

    ניסיון לשיפור ביצועים שלא צלח

--- PlanetanyaUnity/Assets/01 - Room/RoomCamera.cs
+++ PlanetanyaUnity/Assets/01 - Room/RoomCamera.cs
@@ -34,36 +34,40 @@ public class RoomCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount == 2)
+        if (deviceClicked == "")
         {
-            //Debug.Log(Input.touchCount);
-//            fingers1.text = Input.touchCount.ToString();
+            if (Input.touchCount == 2)
+            {
+                //Debug.Log(Input.touchCount);
+                //            fingers1.text = Input.touchCount.ToString();
 
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
 
-            Vector3 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            Vector3 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float currentMaginitude = (touchZero.position - touchOne.position).magnitude;
-            float difference = currentMaginitude - prevMagnitude;
+                Vector3 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                Vector3 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+                float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                float currentMaginitude = (touchZero.position - touchOne.position).magnitude;
+                float difference = currentMaginitude - prevMagnitude;
 
-            //fingers2.text = difference.ToString();
+                //fingers2.text = difference.ToString();
 
-            zoom(difference * -0.01f);
+                zoom(difference * -0.05f);
+            }
+            else if (Input.touchCount == 1 || Input.GetMouseButton(0))
+            {
+                float FOVfactor = Camera.main.fieldOfView / 60;
+                transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed * FOVfactor, -Input.GetAxis("Mouse X") * speed * FOVfactor, 0));
+                X = transform.rotation.eulerAngles.x;
+                Y = transform.rotation.eulerAngles.y;
+                transform.rotation = Quaternion.Euler(X, Y, 0);
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                zoom(Input.GetAxis("Mouse ScrollWheel") * morescroll);
+            }
         }
-        else if (Input.touchCount == 1 || Input.GetMouseButton(0))
-        {
-            float FOVfactor = Camera.main.fieldOfView / 60;
-            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed * FOVfactor, -Input.GetAxis("Mouse X") * speed * FOVfactor, 0));
-            X = transform.rotation.eulerAngles.x;
-            Y = transform.rotation.eulerAngles.y;
-            transform.rotation = Quaternion.Euler(X, Y, 0);
-        }
-        else if (Input.GetAxis("Mouse ScrollWheel") != 0)
-        {
-            zoom(Input.GetAxis("Mouse ScrollWheel") * morescroll);
-        }
+
 
         //if (RoomCamera.deviceClicked != "")
         //{
