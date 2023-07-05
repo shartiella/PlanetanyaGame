24406d262c2079117f5086fd7e0a46a98777f792
shartiella <shartiella@gmail.com>
2023-07-05 16:50:03 +0300

    אנימציית סיום

--- PlanetanyaUnity/Assets/06 - AR Launch/FinalLaunchManager.cs
+++ PlanetanyaUnity/Assets/06 - AR Launch/FinalLaunchManager.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,21 +11,89 @@ public class FinalLaunchManager : MonoBehaviour
     [SerializeField] private GameObject StoryWindow;
     [SerializeField] private GameObject StoryBTN;
 
-    [SerializeField] private GameObject camplace1;
-    [SerializeField] private GameObject camplace2;
-    [SerializeField] private GameObject camplace3;
+    [SerializeField] private CinemachineVirtualCamera Cam;
+
+    private Transform camplace1;
+    private Transform camplace2;
+    private Transform camplace3;
+    [SerializeField] private Transform LEOcamplace1;
+    [SerializeField] private Transform LEOcamplace2;
+    [SerializeField] private Transform LEOcamplace3;
+    [SerializeField] private Transform MEOcamplace1;
+    [SerializeField] private Transform MEOcamplace2;
+    [SerializeField] private Transform MEOcamplace3;
+    [SerializeField] private Transform GEOcamplace1;
+    [SerializeField] private Transform GEOcamplace2;
+    [SerializeField] private Transform GEOcamplace3;
     private int camPos = 1;
 
-    [SerializeField] private GameObject rocketTop;
-    [SerializeField] private GameObject rocketBottom;
-    [SerializeField] private ParticleSystem fire;
+    private GameObject rocketTop;
+    private GameObject rocketBottom;
+    private ParticleSystem fire;
+    [SerializeField] private GameObject rocketLEO;
+    [SerializeField] private GameObject rocketMEO;
+    [SerializeField] private GameObject rocketGEO;
+
+    [SerializeField] private GameObject LEOrocketTop;
+    [SerializeField] private GameObject LEOrocketBottom;
+    [SerializeField] private ParticleSystem LEOfire;
+    [SerializeField] private GameObject MEOrocketTop;
+    [SerializeField] private GameObject MEOrocketBottom;
+    [SerializeField] private ParticleSystem MEOfire;
+    [SerializeField] private GameObject GEOrocketTop;
+    [SerializeField] private GameObject GEOrocketBottom;
+    [SerializeField] private ParticleSystem GEOfire;
+
+    private GameObject target;
+    [SerializeField] private GameObject LEOTarget;
+    [SerializeField] private GameObject MEOTarget;
+    [SerializeField] private GameObject GEOTarget;
 
     private float pushTimer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Globals.ChosenSatellite.Orbit == "LEO")
+        {
+            //target = LEOTarget;
+            rocketTop= LEOrocketTop;
+            rocketBottom= LEOrocketBottom;
+            fire = LEOfire;
+            rocketLEO.SetActive(true);
+            camplace1 = LEOcamplace1;
+            camplace2 = LEOcamplace2;
+            camplace3 = LEOcamplace3;
+            Cam.Follow = LEOcamplace1.transform;
+            Cam.LookAt = LEOTarget.transform;
+            
+        }
+        else if (Globals.ChosenSatellite.Orbit == "MEO")
+        {
+            //target = MEOTarget;
+            rocketTop = MEOrocketTop;
+            rocketBottom = MEOrocketBottom;
+            fire = MEOfire;
+            rocketMEO.SetActive(true);
+            camplace1 = MEOcamplace1;
+            camplace2 = MEOcamplace2;
+            camplace3 = MEOcamplace3;
+            Cam.Follow = MEOcamplace1.transform;
+            Cam.LookAt = MEOTarget.transform;
+        }
+        else if (Globals.ChosenSatellite.Orbit == "GEO")
+        {
+            //target = GEOTarget;
+            rocketTop = GEOrocketTop;
+            rocketBottom = GEOrocketBottom;
+            fire = GEOfire;
+            rocketGEO.SetActive(true);
+            camplace1 = GEOcamplace1;
+            camplace2 = GEOcamplace2;
+            camplace3 = GEOcamplace3;
+            Cam.Follow = GEOcamplace1.transform;
+            Cam.LookAt = GEOTarget.transform;
+        }
     }
 
     // Update is called once per frame
@@ -65,11 +134,76 @@ public class FinalLaunchManager : MonoBehaviour
                 break;
 
             case 2:
-                counter= 3;
+                if (Globals.ChosenSatellite.Orbit == "LEO")
+                {
+                    showStoryWindow("כל הכבוד – עשיתם את זה! בניתם ושיגרתם לוויין מיפוי למסלול נמוך!", true);
+                }
+                else if (Globals.ChosenSatellite.Orbit == "MEO")
+                {
+                    showStoryWindow("כל הכבוד – עשיתם את זה! בניתם ושיגרתם לוויין ניווט למסלול בינוני!", true);
+                }
+                else if (Globals.ChosenSatellite.Orbit == "GEO")
+                {
+                    showStoryWindow("כל הכבוד – עשיתם את זה! בניתם ושיגרתם לוויין תקשורת למסלול גיאוסינכרוני!", true);
+                }
+                //if (typewriterUI.TypeWriterIsFinished)
+                //{
+                //    counter = 3;
+                //}
                 break;
 
             case 3:
-                //changeCamPosition();
+                hideStoryWindow();
+                counter = 4;
+                break;
+
+            case 4:
+                showStoryWindow("זה היה תהליך ארוך ומאתגר, אבל היה שווה את זה!", true);
+                break;
+
+            case 5:
+                hideStoryWindow();
+                changeCamPosition();
+                counter = 6;
+                break;
+
+            case 6:
+                if (Globals.ChosenSatellite.Orbit == "LEO")
+                {
+                    showStoryWindow("לוויינים עומדים מאחורי הרבה דברים בחיי היומיום שלנו, כמו למשל ניווט ותקשורת.", true);
+                }
+                else if (Globals.ChosenSatellite.Orbit == "MEO")
+                {
+                    showStoryWindow("לוויינים עומדים מאחורי הרבה דברים בחיי היומיום שלנו, כמו למשל מיפוי ותקשורת.", true);
+                }
+                else if (Globals.ChosenSatellite.Orbit == "GEO")
+                {
+                    showStoryWindow("לוויינים עומדים מאחורי הרבה דברים בחיי היומיום שלנו, כמו למשל מיפוי וניווט.", true);
+                }
+                break;
+
+            case 7:
+                hideStoryWindow();
+                counter = 8;
+                break;
+
+            case 8:
+                if (Globals.ChosenSatellite.Orbit == "LEO")
+                {
+                    showStoryWindow("אבל מה שחשוב, הוא שעכשיו אתם יודעים איך מצלמים מפות מהחלל!", true);
+                }
+                else if (Globals.ChosenSatellite.Orbit == "MEO")
+                {
+                    showStoryWindow("אבל מה שחשוב, הוא שעכשיו אתם יודעים איך הטלפון יודע איפה השליח נמצא על המפה!", true);
+                }
+                else if (Globals.ChosenSatellite.Orbit == "GEO")
+                {
+                    showStoryWindow("אבל מה שחשוב, הוא שעכשיו אתם יודעים איך הטלוויזיה יודעת מה לשדר לנו!", true);
+                }
+                break;
+
+            case 9:
+                hideStoryWindow();
                 break;
         }
     }
@@ -126,12 +260,12 @@ public class FinalLaunchManager : MonoBehaviour
         if (camPos == 1 && counter == 0)
         {
             camPos = 2;
-            camplace1.LeanMoveLocal(camplace2.transform.localPosition, 10).setDelay(1).setEaseInOutQuad();
+            camplace1.LeanMoveLocal(camplace2.localPosition, 10).setDelay(1).setEaseInOutQuad();
         }
-        else if (camPos == 2 && counter==3) //לשנות קאונטר
+        else if (camPos == 2 && counter==5) //לשנות קאונטר
         {
             camPos = 3;
-            camplace1.LeanMoveLocal(camplace3.transform.localPosition, 10).setDelay(1).setEaseInOutQuad();
+            camplace1.LeanMoveLocal(camplace3.localPosition, 20).setDelay(1).setEaseInOutQuad();
         }
     }
 }
